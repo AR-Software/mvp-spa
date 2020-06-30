2e6d165bb12fa72b2fdf7573e4e9346c945474f7
Roides Javier Cruz Lara <Roides.J.Cruz.-ND@disney.com>
2020-06-30 03:12:46 -0500

    remove assets

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
-import UnderConstructionPage from './pages/UnderConstructionPage/UnderConstructionPage';
 import './App.scss'
 
 export default class AppRoutes extends Component {
@@ -10,11 +9,6 @@ export default class AppRoutes extends Component {
     return (
        <div className="App-container">
           <Route path="/" exact component={HomePage}></Route>
-          <Route path="/skills"  component={UnderConstructionPage}></Route>
-          <Route path="/education" component={UnderConstructionPage}></Route>
-          <Route path="/works"  component={UnderConstructionPage}></Route>
-          <Route path="/research"  component={UnderConstructionPage}></Route>
-          <Route path="/contact" component={UnderConstructionPage}></Route>
         </div>
     );
   }
--- src/pages/UnderConstructionPage/UnderConstructionsConstants.js
+++
@@ -1,8 +0,0 @@
-
-import defaultBackground from '../../assets/images/backgrounds/under_construction.jpg'
-
-export class UnderConstructionsConstants {
-    static TITLE = 'Page under construction';
-    static SUBTITLE = "I'm working hard to show you the best of my work. This content will be available soon!";
-    static BACKGROUND = defaultBackground;
-}
