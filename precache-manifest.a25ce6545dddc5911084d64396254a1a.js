fce5473775b3802bbe80af1d4c7aaad199d8ccf0
Roides Javier Cruz Lara <Roides.J.Cruz.-ND@disney.com>
2020-09-22 02:47:50 -0500

    Updates

---
+++ precache-manifest.a25ce6545dddc5911084d64396254a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a97acd2230e24d9d72",
+    "url": "/mvp-spa/static/css/main.ae877b97.chunk.css"
+  },
+  {
+    "revision": "84a97acd2230e24d9d72",
+    "url": "/mvp-spa/static/js/main.aed4a406.chunk.js"
+  },
+  {
+    "revision": "ee9e0c0d072e4b405a6a",
+    "url": "/mvp-spa/static/js/runtime~main.a1159c16.js"
+  },
+  {
+    "revision": "828eb28883b93270d367",
+    "url": "/mvp-spa/static/js/2.a88e5fac.chunk.js"
+  },
+  {
+    "revision": "131372f0ca59bd1b30e80f625671712f",
+    "url": "/mvp-spa/static/media/beach.131372f0.jpg"
+  },
+  {
+    "revision": "478b108fe4b010221fcb70ef464d59c4",
+    "url": "/mvp-spa/index.html"
+  }
+];
